1feed13da377991af73c1c9238cae882e5fe75a9
MrDem0krat <gavrin.alex@gmail.com>
2016-05-16 06:52:40 +0300

    - Убрана функция, настраивающая логер - Добавлен конфигурационный файл для автоматической настройки логера

--- WeatherServiceHost/Program.cs
+++ WeatherServiceHost/Program.cs
@@ -1,11 +1,8 @@
 ﻿using NLog;
-using NLog.Config;
-using NLog.Targets;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
-using System.Text;
 using WeatherService;
 
 namespace WeatherServiceHost
@@ -25,7 +22,6 @@ namespace WeatherServiceHost
 
                 Console.WriteLine(">>>\tGismeteo Weather Service\t<<<\n");
                 PrintHelp();
-                ConfigLogger();
                 _logger.Debug("Application started");
                 
                 while (isRun)
@@ -264,29 +260,6 @@ namespace WeatherServiceHost
             Console.WriteLine("{0,-20} - Set server for DB connection", Command.Server.ToString().ToLower() + " [server]");
             Console.WriteLine("{0,-20} - Set port for DB connection", Command.Port.ToString().ToLower() + " [port]");
             Console.WriteLine("{0,-20} - Stop service and exit programm\n", Command.Exit.ToString().ToLower());
-        }
-
-        //TODO: Переписать настройки логгера
-        static void ConfigLogger()
-        {
-            LoggingConfiguration config = new LoggingConfiguration();
-
-            FileTarget fileTarget = new FileTarget();
-            config.AddTarget("file", fileTarget);
-
-            fileTarget.DeleteOldFileOnStartup = true; // Удалатья страый файл при запуске
-            fileTarget.KeepFileOpen = false; //Не держать файл открытым
-            fileTarget.ConcurrentWrites = true;
-            fileTarget.Encoding = Encoding.GetEncoding(1251); // Кодировка файла логов
-            fileTarget.ArchiveEvery = FileArchivePeriod.Day; // Период архивирования старых логов (нет смысла, если старый удаляется при запуске)
-            fileTarget.Layout = NLog.Layouts.Layout.FromString("${uppercase:${level}} | ${longdate} :\t${message}"); // Структура сообщения
-            fileTarget.FileName = NLog.Layouts.Layout.FromString("${basedir}/logs/${shortdate}.log"); //Структура названия файлов
-            fileTarget.ArchiveFileName = NLog.Layouts.Layout.FromString("${basedir}/logs/archives/{shortdate}.rar"); // Структура названия архивов
-
-            LoggingRule ruleFile = new LoggingRule("*", LogLevel.Trace, fileTarget); // Минимальный уровень логгирования - Trace
-            config.LoggingRules.Add(ruleFile);
-
-            LogManager.Configuration = config;
         }
     }
 }
